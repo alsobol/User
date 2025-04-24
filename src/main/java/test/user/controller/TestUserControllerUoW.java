0c90b23e1efe27fcad3ba6f9e0a92b850fe6d228
Lera Sobolevskaya <sobolevskaya_valeryia@bk.ru>
2025-04-24 15:38:36 +0300

    some changes update

--- src/main/java/test/user/controller/TestUserControllerUoW.java
+++ src/main/java/test/user/controller/TestUserControllerUoW.java
@@ -6,18 +6,21 @@ import test.user.dao.UnitOfWork;
 import test.user.dao.impl.UnitOfWorkImpl;
 import test.user.dao.impl.UserDaoImpl;
 import test.user.entity.User;
+import test.user.service.UserService;
+import test.user.service.impl.UserServiceImpl;
 
 public class TestUserControllerUoW {
-
+	private static UserService userService = new UserServiceImpl();
 	public static void main(String[] args) {
+		
+		
 
 		User userCreate = new User();
 		userCreate.setAge(15);
 		userCreate.setName("UserUoW");
 		userCreate.setEmail("user.create@unit.work");
 
-		User userModify = new User();
-		userModify.setId(40);
+		User userModify = userService.getUserById(40);
 		userCreate.setAge(16);
 		userCreate.setName("UserModifyUoW");
 		userCreate.setEmail("user.modify@unit.work");
